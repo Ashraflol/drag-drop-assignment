4c6db32128baedfe15195df28b5ecc70064fc168
Ashraflol <ashrafmaniyar19@gmail.com>
2025-09-03 23:11:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Drag and Drop Assignment</h1>
       </header>
+      <Container>
+        <Row>
+          <Col>
+            <div
+              style={{
+                border: "2px dashed #ccc",
+                borderRadius: "5px",
+                padding: "20px",
+                textAlign: "center",
+                minHeight: "200px",
+              }}
+            >
+              <h2>Drag items here</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="text-center">
+            <Button variant="primary" className="m-2">
+              Item 1
+            </Button>
+            <Button variant="secondary" className="m-2">
+              Item 2
+            </Button>
+            <Button variant="success" className="m-2">
+              Item 3
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
